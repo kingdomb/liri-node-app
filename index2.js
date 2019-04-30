5202df3ddff67cfa32adc90b0ceba0a89e3c40d1
King Conch <kbmajor80@gmail.com>
2019-04-30 07:54:38 -0400

    WIP:

---
+++ index2.js
@@ -0,0 +1,33 @@
+let axios = require("axios");
+let title = process.argv[2];
+let movieUrl = "http://www.omdbapi.com/?t=" + title + "&apikey=3733b2b3";
+
+
+// Run the axios.get function...
+// The axios.get function takes in a URL and returns a promise (just like $.ajax)
+axios.get(movieUrl).then(
+    function(response) {
+      // If the axios was successful...
+      // Then log the body from the site!
+      console.log(response.data);
+    //   console.log(response.data[0].venue.city);
+    //   console.log(response.data[0].datetime);
+  
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
